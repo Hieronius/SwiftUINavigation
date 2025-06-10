ace7e8e37106f95e6c469f2edd43b9e80091c277
Arsenty Halimovsky <ghost12@yandex.ru>
2025-06-10 07:36:39 +0300

    Instantiate the project

---
+++ SwiftUINavigation/SwiftUINavigation/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  SwiftUINavigation
+//
+//  Created by Арсентий Халимовский on 10.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+	
+    var body: some View {
+
+    }
+}
---
+++ SwiftUINavigation/SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Арсентий Халимовский on 10.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
